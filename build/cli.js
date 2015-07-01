db5c00a5315a2bfc015f074e3cf855c78eb86448
Martin Camacho <martin@mcamac.com>
2015-07-01 15:46:15 -0400

    Clean up, actually add test file

--- build/cli.js
+++ build/cli.js
@@ -1,50 +1,56 @@
 #!/usr/bin/env node
 "use strict";
 
-var program = require("commander");
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
 
-var _require = require("child_process");
+var program = _interopRequire(require("commander"));
 
-var exec = _require.exec;
+var exec = require("child_process").exec;
 
-var kexec = require("kexec");
-var R = require("ramda");
+var kexec = _interopRequire(require("kexec"));
+
+var R = _interopRequire(require("ramda"));
 
 var resolve = require("path").resolve;
 
-program.version("0.1.0").option("-c, --config [config]", "Config location").parse(process.argv);
+program.version("0.1.0").option("-c, --config [config]", "Config location").option("-n, --just-print", "Just print tmux command").parse(process.argv);
 
 function parseTmuxConfig(config) {
-  console.log(config);
   var sessionCmd = "new-session -s \"" + config.title + "\"";
   var subcommands = [sessionCmd];
 
   R.forEach(function (window) {
     var windowCommands = [];
     windowCommands.push("new-window -n \"" + window.title + "\" -c \"" + config.root + "\"");
+
+    // Create panes
     var paneCommands = R.times(function (paneCommand) {
       return "split-window -v -c \"" + config.root + "\"";
     }, window.panes.length - 1);
     paneCommands = R.intersperse("select-pane -D", paneCommands);
-    console.log(window, windowCommands, paneCommands);
-    windowCommands = windowCommands.concat(paneCommands);
-    windowCommands.push("select-layout " + window.layout);
-    windowCommands.push("select-pane -t 0");
-    window.panes.forEach(function (pane) {
-      var escapedPane = pane.replace(/"/g, "\\\"");
-      windowCommands.push("send-keys \"" + escapedPane + "\" \"Enter\"");
-      windowCommands.push("select-pane -D");
+    windowCommands.push(paneCommands);
+
+    // Choose layout and select top pane
+    windowCommands.push(["select-layout " + window.layout, "select-pane -t 0"]);
+
+    // Send keys to each pane in order
+    window.panes.forEach(function (paneCommand) {
+      var escapedPaneCommand = paneCommand.replace(/"/g, "\\\"");
+      windowCommands.push(["send-keys \"" + escapedPaneCommand + "\" \"Enter\"", "select-pane -D"]);
     });
     subcommands.push(windowCommands);
   }, config.windows);
 
   subcommands.push("kill-window -t 0");
-  // subcommands.push(`sek-window -t 0`)
 
   var command = "tmux " + R.flatten(subcommands).join(" \\; ");
-  console.log(command);
   return command;
 }
 
-kexec(parseTmuxConfig(require(resolve(process.cwd(), program.config))));
+var tmuxCommand = parseTmuxConfig(require(resolve(process.cwd(), program.config)));
+if (program.justPrint) {
+  console.log(tmuxCommand);
+} else {
+  kexec(tmuxCommand);
+}
 
--- src/cli.js
+++ src/cli.js
@@ -1,46 +1,56 @@
 #!/usr/bin/env node
 
-var program = require('commander')
-var {exec} = require('child_process')
-var kexec = require('kexec')
-var R = require('ramda')
-import {resolve} from 'path';
+import program from 'commander'
+import {exec} from 'child_process'
+import kexec from 'kexec'
+import R from 'ramda'
+import {resolve} from 'path'
 
 program
   .version('0.1.0')
   .option('-c, --config [config]', 'Config location')
+  .option('-n, --just-print', 'Just print tmux command')
   .parse(process.argv)
 
 function parseTmuxConfig(config) {
-  console.log(config)
   var sessionCmd = `new-session -s "${config.title}"`
   var subcommands = [sessionCmd]
 
   R.forEach((window) => {
     var windowCommands = []
     windowCommands.push(`new-window -n "${window.title}" -c "${config.root}"`)
+
+    // Create panes
     var paneCommands = R.times((paneCommand) => {
       return `split-window -v -c "${config.root}"`
     }, window.panes.length - 1)
     paneCommands = R.intersperse('select-pane -D', paneCommands)
-    console.log(window, windowCommands, paneCommands)
-    windowCommands = windowCommands.concat(paneCommands)
-    windowCommands.push(`select-layout ${window.layout}`)
-    windowCommands.push(`select-pane -t 0`)
-    window.panes.forEach((pane) => {
-      var escapedPane = pane.replace(/"/g, '\\\"')
-      windowCommands.push(`send-keys "${escapedPane}" "Enter"`)
-      windowCommands.push(`select-pane -D`)
+    windowCommands.push(paneCommands)
+
+    // Choose layout and select top pane
+    windowCommands.push([
+      `select-layout ${window.layout}`,
+      `select-pane -t 0`])
+
+    // Send keys to each pane in order
+    window.panes.forEach((paneCommand) => {
+      var escapedPaneCommand = paneCommand.replace(/"/g, '\\\"')
+      windowCommands.push([
+        `send-keys "${escapedPaneCommand}" "Enter"`,
+        `select-pane -D`])
     })
     subcommands.push(windowCommands)
   }, config.windows)
 
   subcommands.push(`kill-window -t 0`)
-  // subcommands.push(`sek-window -t 0`)
 
   var command = 'tmux ' + R.flatten(subcommands).join(' \\; ')
-  console.log(command);
   return command;
 }
 
-kexec(parseTmuxConfig(require(resolve(process.cwd(), program.config))))
+var tmuxCommand = parseTmuxConfig(require(resolve(process.cwd(), program.config)))
+if (program.justPrint) {
+  console.log(tmuxCommand)
+} else {
+  kexec(tmuxCommand)
+}
